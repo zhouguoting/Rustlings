f080f9442197eeefea7086b9f324d504b9e2f8d1
zhouguoting <zhouguoting@zuzuche.com>
2025-01-20 13:53:50 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(weight: i32) -> i32 {
+    if weight > 40 {
+        weight * 1
+    } else {
+        weight * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
